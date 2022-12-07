0e71f48edd30380d4ca3c7a088456594bd086e0f
Brian Clifford <bcliff323@gmail.com>
2022-12-07 16:04:35 -0500

    Fetch contentful data

---
+++ src/_data/recipes.js
@@ -0,0 +1,23 @@
+const contentful = require("contentful");
+
+const client = contentful.createClient({
+	space: process.env.CTFL_SPACE,
+	accessToken: process.env.CTFL_ACCESSTOKEN
+});
+
+module.exports = async () => {
+	return client.getEntries({ content_type: 'recipe', order: 'sys.createdAt' })
+		.then(function (response) {
+			return response.items.map(recipe => ({
+				tags: recipe.metadata.tags.map(t => t.sys.id),
+				id: recipe.sys.id,
+				createdAt: recipe.sys.createdAt,
+				updatedAt: recipe.sys.updatedAt,
+				title: recipe.fields.title,
+				author: recipe.fields.author,
+				description: recipe.fields.description,
+				ingredients: recipe.fields.ingredients,
+				steps: recipe.fields.steps
+			}));
+		}).catch(console.error);
+};
